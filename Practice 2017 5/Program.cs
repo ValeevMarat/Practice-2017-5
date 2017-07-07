31c2f93fa9f2ca6d9c2ab1a780865bac24d7df6d
Marat <user@LAPTOP-7JKVIGGR>
2017-07-07 10:12:45 +0500

    Код написан и прокомментирован. Зашиты от "дурака" нет

--- Practice 2017 5/Program.cs
+++ Practice 2017 5/Program.cs
@@ -8,9 +8,42 @@ namespace Practice_2017_5
 {
     class Program
     {
+        static double[,] ReadMatrix(int n)
+        {
+            double[,] matrix =new double[n,n];
+             
+            for (int i = 0; i < n; i++)          // Обход всех рядов для записи
+            {
+                Console.WriteLine("Введите ВСЮ " + (i + 1) + "-ую строку (отделяя числа одним пробелом): ");
+                string row = Console.ReadLine();
+
+                for (int j = 0; j < n; j++)      // Обход всех столбцов для записи
+                    matrix[i, j] = double.Parse(row.Split(' ')[j]);
+            }
+            return matrix;
+        } // Чтение матрицы
+
+        static double MatrixsLeftBottomPartMax(double[,]matrix)
+        {
+            int n = matrix.GetLength(0);
+            double max = int.MinValue;
+
+            for (int i = 0; i < n; i++)              // Обход всех рядов
+                for (int j = n - 1 - i; j < n; j++)  // Обход столбцов из заданной части матрицы (правый нижний угол вместе с диагональю)
+                    if (matrix[i, j] > max)
+                        max = matrix[i, j];
+            return max;
+        } // Поиск максимума из заданной части матрицы (правый нижний угол вместе с диагональю)
+
         static void Main(string[] args)
         {
+            Console.Write("Введите размерность матрицы: ");
+            int n = int.Parse(Console.ReadLine());
 
+            double[,] matrix = ReadMatrix(n);
+            
+            Console.WriteLine("Максимальное число из заданной части матрицы = "+ MatrixsLeftBottomPartMax(matrix));
+            Console.ReadLine();
         }
     }
 }
